2ec9f57459331525e25e1b27ebe7e81d821bf0da
skparab1 <skparab1@gmail.com>
2023-02-04 12:24:29 -0800

    yea

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // this is the js
+function clearall(){
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('aboutme').style.display = 'none';
+    document.getElementById('discordmod').style.display = 'none';
+    document.getElementById('socials').style.display = 'none';
+    document.getElementById('lol').style.display = 'none';
+}
+
+function appear(id){
+    document.getElementById(id).style.display = 'block';
+}
